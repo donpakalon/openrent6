3c5e6a4e2222912787de5affc536dacaef386d5e
Ali Abou Daher <aliad9802@hotmail.com>
2024-09-05 12:03:13 +0200

    rental show and index

---
+++ app/controllers/rentals_controller.rb
@@ -0,0 +1,10 @@
+class RentalsController < ApplicationController
+  def index
+    ## get the rentals of the curent user
+    @rentals = current_user.rentals
+  end
+
+  def show
+    @rental = Rental.find(params[:id])
+  end
+end
